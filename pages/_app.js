0ea3a1c157c52eae6ca8ae068e58dfab59606f13
kallolo <mustajib.sif@gmail.com>
2021-02-04 23:01:06 +0700

    install react-query dan belajar menggunakannya

--- pages/_app.js
+++ pages/_app.js
@@ -1,40 +1,47 @@
 import '../styles/globals.css'
 import Head from 'next/head'
 import { ThemeProvider } from 'next-themes'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
+const queryClient = new QueryClient();
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
-    <ThemeProvider attribute="class">
-       <Head>
-        <meta charSet="utf-8" />
-        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <meta
-          name="viewport"
-          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
-        />
-        <meta name="description" content="Description" />
-        <meta name="keywords" content="Keywords" />
-        <title>Next.js PWA Example</title>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider attribute="class">
+          <Head>
+            <meta charSet="utf-8" />
+            <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+            <meta
+              name="viewport"
+              content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+            />
+            <meta name="description" content="Description" />
+            <meta name="keywords" content="Keywords" />
+            <title>Next.js PWA Example</title>
 
-        <link rel="manifest" href="/manifest.json" />
-        <link
-          href="/icons/favicon-16x16.png"
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-        />
-        <link
-          href="/icons/favicon-32x32.png"
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-        />
-        <link rel="apple-touch-icon" href="/apple-icon.png"></link>
-        <meta name="theme-color" content="#317EFB" />
-      </Head>
-      <Component {...pageProps} />
-    </ThemeProvider>
+            <link rel="manifest" href="/manifest.json" />
+            <link
+              href="/icons/favicon-16x16.png"
+              rel="icon"
+              type="image/png"
+              sizes="16x16"
+            />
+            <link
+              href="/icons/favicon-32x32.png"
+              rel="icon"
+              type="image/png"
+              sizes="32x32"
+            />
+            <link rel="apple-touch-icon" href="/apple-icon.png"></link>
+            <meta name="theme-color" content="#317EFB" />
+          </Head>
+          <Component {...pageProps} />
+        </ThemeProvider>
+        <ReactQueryDevtools/>
+      </QueryClientProvider>
+
     </>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,62 @@
 import Layout from '../components/Layout'
+import { useQuery } from 'react-query';
+
+const getMarket = async () =>{ // fungsi get data
+  const URL = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=idr&per_page=10";
+  const response = await fetch(URL);
+  if(!response.ok){
+    throw new Error("Fetching Error");
+  }
+  return await response.json();
+};
+
+const formatNumber = (number) =>{ // fungsi format number indonesia
+  return Intl.NumberFormat("id-Id").format(number);
+}
+
+const Percentage = ({percent}) =>{ // componen percentage
+  const formatPercent = Intl.NumberFormat("id-Id",{
+    style: "percent",
+    maximumFractionDigits:1,
+    minimumFractionDigits:1
+  }).format(percent / 100);
+
+  let color = "text-black"
+  if(percent > 0){
+    color = "text-green-500"
+  }else{
+    color = "text-red-500"
+  }
+
+  return <span className={color}>{formatPercent}</span>
+}
 export default function Home() {
+  const { data, isError, isLoading, isFetching, isSuccess } = useQuery("market", getMarket)
   return (
     <Layout title={'Home # ' + process.env.appName}>
       <div className="container">
         <h1 className="text-lg font-bold text-blue-600"> Home</h1>
+        <table className="table-auto border-collapse  border border-gray-400 dark:border-white">
+          <thead className="text-left uppercase">
+            <tr>
+              <th className="border border-gray-400 dark:border-white p-2">Coin</th>
+              <th className="border border-gray-400 dark:border-white p-2">Last Price</th>
+              <th className="border border-gray-400 dark:border-white p-2">24h % Change</th>
+            </tr>
+          </thead>
+          <tbody>
+          {
+          isSuccess && // kalau sudah success data dimap seperti dibawah
+          data?.map((r,i) => ( // ? => jika datanya ada baru dimap
+            <tr>
+              <td className="border border-gray-400 dark:border-white p-2">{r.id}</td>
+              <td className="border border-gray-400 dark:border-white p-2">{formatNumber(r.current_price) }</td>
+              <td className="border border-gray-400 dark:border-white p-2"><Percentage percent={r.price_change_percentage_24h}/></td>
+            </tr>
+          ))}
+          </tbody>
+        </table>
+        
       </div>
     </Layout>
   )
